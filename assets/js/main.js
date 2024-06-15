41a6fd4cee617a79b9a09d48e287eef0fef3cef7
Mashiur Rahman <mashiur.dev@gmail.com>
2024-06-15 09:34:52 +0600

    rypt-5 and 6 added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
   /**
    * Single SLider
    */
@@ -43,19 +42,68 @@ $(document).ready(function () {
     asNavFor: ".slide-thumb",
   });
 
-  $(".slide-thumb")
-    .slick({
-      slidesToShow: 1,
-      centerMode: true,
-      centerPadding: "30%",
-      slidesToScroll: 1,
-      asNavFor: ".slide-preview",
-      dots: true,
-      arrows: false,
-      focusOnSelect: true,
-    });
-  
+  $(".slide-thumb").slick({
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: "30%",
+    slidesToScroll: 1,
+    asNavFor: ".slide-preview",
+    dots: true,
+    arrows: false,
+    focusOnSelect: true,
+  });
 
+  /**
+   * Customized box preview
+   */
 
+  $(".customized-addons-slide").slick({
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: "18%",
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    focusOnSelect: true,
+  });
 
+  /**
+   * Add title on Box
+   */
+  let titlePrviewBox = $(".customized-box-preview-inner img");
+  let checkBox = $(".box-title input");
+
+  checkBox.on("click", function () {
+    checkBox.prop("checked", false);
+    $(this).prop("checked", true);
+
+    let getTitle = $(this).data("title");
+
+    titlePrviewBox.attr(
+      "src",
+      "assets/images/slide-marlboro-with-mic-" + getTitle + ".png"
+    );
+  });
+
+  /**
+   * Background Image checkbox
+   */
+  let boxBgPreview = $(".box-bg-addon .bg-preview");
+  let boxBgcheckBox = $(".box-bg-addon .item-inner input");
+
+  boxBgcheckBox.on('click', function () {
+    boxBgcheckBox.prop("checked", false);
+    $(this).prop("checked", true);
+
+    let getBg = $(this).data("background");
+
+    console.log(getBg);
+
+    boxBgPreview.attr(
+      "style",
+      "background-image: url(assets/images/"+ getBg +");"
+    );
+  });
+
+  //
 });
