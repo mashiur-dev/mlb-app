1df41e69b70cc8ec47c145df12b05f5e056cbe88
Mashiur Rahman <mashiur.dev@gmail.com>
2024-06-21 03:56:37 +0600

    all page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,87 +2,106 @@ $(document).ready(function () {
   /**
    * Single SLider
    */
-  $("#single-carousel").slick({
-    centerMode: true,
-    // centerPadding: "60px",
-    slidesToShow: 1,
-    arrows: false,
-    centerPadding: "25%",
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: "20%",
-          slidesToShow: 1,
+  if ($("#single-carousel").length) {
+    $("#single-carousel").slick({
+      centerMode: true,
+      // centerPadding: "60px",
+      slidesToShow: 1,
+      arrows: false,
+      centerPadding: "25%",
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: "20%",
+            slidesToShow: 1,
+          },
         },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: "20%",
-          slidesToShow: 1,
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: "20%",
+            slidesToShow: 1,
+          },
         },
-      },
-    ],
-  });
+      ],
+    });
+  }
 
   /**
    *  Dual Slider
    */
+  if ($(".slide-preview").length && $(".slide-thumb").length) {
+    $(".slide-preview").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: ".slide-thumb",
+    });
 
-  $(".slide-preview").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: ".slide-thumb",
-  });
-
-  $(".slide-thumb").slick({
-    slidesToShow: 1,
-    centerMode: true,
-    centerPadding: "30%",
-    slidesToScroll: 1,
-    asNavFor: ".slide-preview",
-    dots: true,
-    arrows: false,
-    focusOnSelect: true,
-  });
+    $(".slide-thumb").slick({
+      slidesToShow: 1,
+      centerMode: true,
+      centerPadding: "30%",
+      slidesToScroll: 1,
+      asNavFor: ".slide-preview",
+      dots: true,
+      arrows: false,
+      focusOnSelect: true,
+    });
+  }
 
   /**
    * Customized box preview
    */
 
-  $(".customized-addons-slide").slick({
-    slidesToShow: 1,
-    centerMode: true,
-    centerPadding: "18%",
-    slidesToScroll: 1,
-    dots: true,
-    arrows: false,
-    focusOnSelect: true,
-  });
+  if ($(".customized-addons-slide").length) {
+    $(".customized-addons-slide").slick({
+      slidesToShow: 1,
+      centerMode: true,
+      centerPadding: "18%",
+      slidesToScroll: 1,
+      dots: true,
+      arrows: false,
+      focusOnSelect: true,
+    });
+  }
 
   /**
    * Add title on Box
    */
   let titlePrviewBox = $(".customized-box-preview-inner img");
-  let checkBox = $(".box-title input");
+  //let checkBox = $(".box-title input");
+  let checkBox = $(".green-checkbox input");
 
   checkBox.on("click", function () {
+    console.log("click");
+
     checkBox.prop("checked", false);
     $(this).prop("checked", true);
 
-    let getTitle = $(this).data("title");
+    if (titlePrviewBox.length) {
 
-    titlePrviewBox.attr(
-      "src",
-      "assets/images/slide-marlboro-with-mic-" + getTitle + ".png"
-    );
+      console.log('exist');
+
+      let getTitle = $(this).data("title");
+
+      titlePrviewBox.attr(
+        "src",
+        "assets/images/slide-marlboro-with-mic-" + getTitle + ".png"
+      );
+
+    } else {
+
+      console.log("not exist");
+
+    }
+    
   });
 
   /**
